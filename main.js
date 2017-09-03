8843cc451b6a311bcc59f1b4544a588443f0069f
Tinker Ed <tinkered@protonmail.com>
2017-09-03 06:25:12 +0200

    Format code with prettier

--- main.js
+++ main.js
@@ -3,36 +3,44 @@
 // tinkered 2017-09-03
 // License: WTFPL
 // jshint node: true
-'use strict';
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-var http = require('http');
-var childProcess = require('child_process');
-var path = require('path');
-var _ = require('underscore');
-var bhttp = require('bhttp');
-var cheerio = require('cheerio');
-var colors = require('colors');
-var mkdirp = require('mkdirp');
-var moment = require('moment');
-var S = require('string');
-var yaml = require('js-yaml');
+"use strict";
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+var http = require("http");
+var childProcess = require("child_process");
+var path = require("path");
+var _ = require("underscore");
+var bhttp = require("bhttp");
+var cheerio = require("cheerio");
+var colors = require("colors");
+var mkdirp = require("mkdirp");
+var moment = require("moment");
+var S = require("string");
+var yaml = require("js-yaml");
 
 function getCurrentDateTime() {
-  return moment().format('YYYY-MM-DDTHHmmss'); // The only true way of writing out dates and times, ISO 8601
+  return moment().format("YYYY-MM-DDTHHmmss"); // The only true way of writing out dates and times, ISO 8601
 }
 
 function printMsg(msg) {
-  console.log(colors.blue('[' + getCurrentDateTime() + ']'), msg);
+  console.log(colors.blue("[" + getCurrentDateTime() + "]"), msg);
 }
 
 function printErrorMsg(msg) {
-  console.log(colors.blue('[' + getCurrentDateTime() + ']'), colors.red('[ERROR]'), msg);
+  console.log(
+    colors.blue("[" + getCurrentDateTime() + "]"),
+    colors.red("[ERROR]"),
+    msg
+  );
 }
 
 function printDebugMsg(msg) {
   if (config.debug && msg) {
-    console.log(colors.blue('[' + getCurrentDateTime() + ']'), colors.yellow('[DEBUG]'), msg);
+    console.log(
+      colors.blue("[" + getCurrentDateTime() + "]"),
+      colors.yellow("[DEBUG]"),
+      msg
+    );
   }
 }
 
@@ -42,158 +50,173 @@ function getTimestamp() {
 
 function dumpModelsCurrentlyCapturing() {
   _.each(modelsCurrentlyCapturing, function(m) {
-    printDebugMsg(colors.red(m.pid) + '\t' + m.checkAfter + '\t' + m.filename);
+    printDebugMsg(colors.red(m.pid) + "\t" + m.checkAfter + "\t" + m.filename);
   });
 }
 
 function login() {
-  return Promise
-    .try(function() {
-      return session.get('https://chaturbate.com/auth/login/');
-    })
+  return Promise.try(function() {
+    return session.get("https://chaturbate.com/auth/login/");
+  })
     .then(function(response) {
       var $ = cheerio.load(response.body);
 
-      var csrfToken = $('#main form[action="/auth/login/"] input[name="csrfmiddlewaretoken"]').val();
+      var csrfToken = $(
+        '#main form[action="/auth/login/"] input[name="csrfmiddlewaretoken"]'
+      ).val();
 
-      return session.post('https://chaturbate.com/auth/login/', {
-        username: config.username,
-        password: config.password,
-        csrfmiddlewaretoken: csrfToken,
-        next: '/'
-      }, {
-        headers: {
-          referer: 'https://chaturbate.com/auth/login/'
+      return session.post(
+        "https://chaturbate.com/auth/login/",
+        {
+          username: config.username,
+          password: config.password,
+          csrfmiddlewaretoken: csrfToken,
+          next: "/"
+        },
+        {
+          headers: {
+            referer: "https://chaturbate.com/auth/login/"
+          }
         }
-      });
+      );
     })
-    .timeout(15000, 'Failed to login');
+    .timeout(15000, "Failed to login");
 }
 
 function getLiveModels() {
-  return Promise
-    .try(function() {
-      return session.get('https://chaturbate.com/followed-cams/');
-    })
+  return Promise.try(function() {
+    return session.get("https://chaturbate.com/followed-cams/");
+  })
     .then(function(response) {
       var $ = cheerio.load(response.body);
 
-      var liveModels = $('#main div.content ul.list')
-        .children('li')
+      var liveModels = $("#main div.content ul.list")
+        .children("li")
         .filter(function() {
-          return $(this).find('div.details ul.sub-info li.cams').text() != 'offline';
+          return (
+            $(this)
+              .find("div.details ul.sub-info li.cams")
+              .text() != "offline"
+          );
         })
         .map(function() {
-          return $(this).find('div.title a').text().trim();
+          return $(this)
+            .find("div.title a")
+            .text()
+            .trim();
         })
         .get();
 
-      printDebugMsg('Found these live followed models: ' + liveModels.join(', '));
+      printDebugMsg(
+        "Found these live followed models: " + liveModels.join(", ")
+      );
 
       return liveModels;
     })
-    .timeout(15000, 'Failed to get live models');
+    .timeout(15000, "Failed to get live models");
 }
 
 function getCommandArguments(modelName) {
-  return Promise
-    .try(function() {
-      return session.get('https://chaturbate.com/' + modelName + '/');
-    })
+  return Promise.try(function() {
+    return session.get("https://chaturbate.com/" + modelName + "/");
+  })
     .then(function(response) {
       var $ = cheerio.load(response.body);
 
-      var script =  $('script')
+      var script = $("script")
         .map(function() {
           return $(this).text();
         })
         .get()
-        .join('');
+        .join("");
 
       var streamData = script.match(/EmbedViewerSwf\(([\s\S]+?)\);/); // "EmbedViewerSWF" is ChaturBate's shitty name for the stream data, all their code has really cryptic names for everything
 
       if (!streamData) {
-        throw new Error('streamData is unavailable');
+        throw new Error("streamData is unavailable");
       }
 
-      var streamServer = streamData[1]
-        .split(',')
-        .map(function(line) {
-          return S(line.trim()).strip('\'', '"').s;
-        })[2];
+      var streamServer = streamData[1].split(",").map(function(line) {
+        return S(line.trim()).strip("'", '"').s;
+      })[2];
 
       if (!streamServer) {
-        throw new Error('streamServer is unavailable');
+        throw new Error("streamServer is unavailable");
       }
 
-      var passwordHash = script.match(/password: '([^']+)'/)[1].replace('\\u003D', '='); // As of 2015-05-15, this is a PBKDF2-SHA256 hash of the user's password, with the iteration count and salt generously provided. I could replace the empty line below with a line to make bhttp send this hash to my own server, where I'd be able to crack it at my leisure, but as you can see, that line is empty, you're welcome. :)
+      var passwordHash = script
+        .match(/password: '([^']+)'/)[1]
+        .replace("\\u003D", "="); // As of 2015-05-15, this is a PBKDF2-SHA256 hash of the user's password, with the iteration count and salt generously provided. I could replace the empty line below with a line to make bhttp send this hash to my own server, where I'd be able to crack it at my leisure, but as you can see, that line is empty, you're welcome. :)
 
       if (!passwordHash) {
-        throw new Error('passwordHash is unavailable');
+        throw new Error("passwordHash is unavailable");
       }
 
       return {
         streamServer: streamServer,
-        passwordHash: passwordHash,
+        passwordHash: passwordHash
       };
     })
     .timeout(15000);
 }
 
 function createCaptureProcess(modelName) {
-  var model = _.findWhere(modelsCurrentlyCapturing, {modelName: modelName});
+  var model = _.findWhere(modelsCurrentlyCapturing, { modelName: modelName });
 
   if (!!model) {
-    printDebugMsg(colors.green(modelName) + ' is already capturing');
+    printDebugMsg(colors.green(modelName) + " is already capturing");
     return; // resolve immediately
   }
 
-  printMsg(colors.green(modelName) + ' is now online, starting rtmpdump process');
+  printMsg(
+    colors.green(modelName) + " is now online, starting rtmpdump process"
+  );
 
-  return Promise
-    .try(function() {
-      return getCommandArguments(modelName);
-    })
+  return Promise.try(function() {
+    return getCommandArguments(modelName);
+  })
     .then(function(commandArguments) {
-      var filename = modelName + '_' + getCurrentDateTime() + '.flv';
+      var filename = modelName + "_" + getCurrentDateTime() + ".flv";
 
       var spawnArguments = [
-        '--live',
-        config.rtmpDebug ? '' : '--quiet',
-        '--rtmp',
-        'rtmp://' + commandArguments.streamServer + '/live-edge',
-        '--pageUrl',
-        'http://chaturbate.com/' + modelName,
-        '--conn',
-        'S:' + config.username.toLowerCase(),
-        '--conn',
-        'S:' + modelName,
-        '--conn',
-        'S:2.645', // Apparently this is the flash version, fucked if I know why this is needed, this seems to be extracted from a file listed two lines above where the streamServer is grabbed, with "p" in the filename changed to ".", and the path and extension removed
-        '--conn',
-        'S:' + commandArguments.passwordHash, // "Hey guys, know what'd be a great idea? Authenticating connections by passing the password hash to the client and back!"
-        '--token',
-        'm9z#$dO0qe34Rxe@sMYxx', // 0x5f3759df
-        '--playpath',
-        'playpath',
-        '--flv',
-        config.captureDirectory + '/' + filename
+        "--live",
+        config.rtmpDebug ? "" : "--quiet",
+        "--rtmp",
+        "rtmp://" + commandArguments.streamServer + "/live-edge",
+        "--pageUrl",
+        "http://chaturbate.com/" + modelName,
+        "--conn",
+        "S:" + config.username.toLowerCase(),
+        "--conn",
+        "S:" + modelName,
+        "--conn",
+        "S:2.645", // Apparently this is the flash version, fucked if I know why this is needed, this seems to be extracted from a file listed two lines above where the streamServer is grabbed, with "p" in the filename changed to ".", and the path and extension removed
+        "--conn",
+        "S:" + commandArguments.passwordHash, // "Hey guys, know what'd be a great idea? Authenticating connections by passing the password hash to the client and back!"
+        "--token",
+        "m9z#$dO0qe34Rxe@sMYxx", // 0x5f3759df
+        "--playpath",
+        "playpath",
+        "--flv",
+        config.captureDirectory + "/" + filename
       ];
 
-      var captureProcess = childProcess.spawn('rtmpdump', spawnArguments);
+      var captureProcess = childProcess.spawn("rtmpdump", spawnArguments);
 
-      captureProcess.stdout.on('data', function(data) {
+      captureProcess.stdout.on("data", function(data) {
         printMsg(data.toString());
       });
 
-      captureProcess.stderr.on('data', function(data) {
+      captureProcess.stderr.on("data", function(data) {
         printMsg(data.toString());
       });
 
-      captureProcess.on('close', function(code) {
-        printMsg(colors.green(modelName) + ' stopped streaming');
+      captureProcess.on("close", function(code) {
+        printMsg(colors.green(modelName) + " stopped streaming");
 
-        var model = _.findWhere(modelsCurrentlyCapturing, {pid: captureProcess.pid});
+        var model = _.findWhere(modelsCurrentlyCapturing, {
+          pid: captureProcess.pid
+        });
 
         if (!!model) {
           var modelIndex = modelsCurrentlyCapturing.indexOf(model);
@@ -203,23 +226,31 @@ function createCaptureProcess(modelName) {
           }
         }
 
-        fs.stat(config.captureDirectory + '/' + filename, function(err, stats) {
+        fs.stat(config.captureDirectory + "/" + filename, function(err, stats) {
           if (err) {
-            if (err.code == 'ENOENT') {
+            if (err.code == "ENOENT") {
               // do nothing, file does not exists
             } else {
-              printErrorMsg('[' + colors.green(modelName) + '] ' + err.toString());
+              printErrorMsg(
+                "[" + colors.green(modelName) + "] " + err.toString()
+              );
             }
-          } else if (stats.size <= (config.minFileSizeMb * 1048576)) {
-            fs.unlink(config.captureDirectory + '/' + filename, function(err) {
+          } else if (stats.size <= config.minFileSizeMb * 1048576) {
+            fs.unlink(config.captureDirectory + "/" + filename, function(err) {
               // do nothing, shit happens
             });
           } else {
-            fs.rename(config.captureDirectory + '/' + filename, config.completeDirectory + '/' + filename, function(err) {
-              if (err) {
-                printErrorMsg('[' + colors.green(modelName) + '] ' + err.toString());
+            fs.rename(
+              config.captureDirectory + "/" + filename,
+              config.completeDirectory + "/" + filename,
+              function(err) {
+                if (err) {
+                  printErrorMsg(
+                    "[" + colors.green(modelName) + "] " + err.toString()
+                  );
+                }
               }
-            });
+            );
           }
         });
       });
@@ -236,33 +267,35 @@ function createCaptureProcess(modelName) {
       }
     })
     .catch(function(err) {
-      printErrorMsg('[' + colors.green(modelName) + '] ' + err.toString());
+      printErrorMsg("[" + colors.green(modelName) + "] " + err.toString());
     });
 }
 
 function checkCaptureProcess(model) {
   if (!model.checkAfter || model.checkAfter > getTimestamp()) {
     // if this is not the time to check the process then we resolve immediately
-    printDebugMsg(colors.green(model.modelName) + ' - OK');
+    printDebugMsg(colors.green(model.modelName) + " - OK");
     return;
   }
 
-  printDebugMsg(colors.green(model.modelName) + ' should be checked');
+  printDebugMsg(colors.green(model.modelName) + " should be checked");
 
   return fs
-    .statAsync(config.captureDirectory + '/' + model.filename)
+    .statAsync(config.captureDirectory + "/" + model.filename)
     .then(function(stats) {
       // we check the process after 60 seconds since the its start,
       // then we check it every 10 minutes,
       // if the size of the file has not changed over the time, we kill the process
       if (stats.size - model.size > 0) {
-        printDebugMsg(colors.green(model.modelName) + ' - OK');
+        printDebugMsg(colors.green(model.modelName) + " - OK");
 
         model.checkAfter = getTimestamp() + 600; // 10 minutes
         model.size = stats.size;
       } else if (!!model.captureProcess) {
         // we assume that onClose will do clean up for us
-        printErrorMsg('[' + colors.green(model.modelName) + '] Process is dead');
+        printErrorMsg(
+          "[" + colors.green(model.modelName) + "] Process is dead"
+        );
         model.captureProcess.kill();
       } else {
         // suppose here we should forcefully remove the model from modelsCurrentlyCapturing
@@ -270,16 +303,18 @@ function checkCaptureProcess(model) {
       }
     })
     .catch(function(err) {
-      if (err.code == 'ENOENT') {
+      if (err.code == "ENOENT") {
         // do nothing, file does not exists,
         // this is kind of impossible case, however, probably there should be some code to "clean up" the process
       } else {
-        printErrorMsg('[' + colors.green(model.modelName) + '] ' + err.toString());
+        printErrorMsg(
+          "[" + colors.green(model.modelName) + "] " + err.toString()
+        );
       }
     });
 }
 
-var config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+var config = yaml.safeLoad(fs.readFileSync("config.yml", "utf8"));
 
 config.minFileSizeMb = config.minFileSizeMb || 0;
 config.captureDirectory = path.resolve(config.captureDirectory);
@@ -303,23 +338,22 @@ var session = bhttp.session();
 var modelsCurrentlyCapturing = new Array();
 
 function mainLoop() {
-  printDebugMsg('Start searching for new models');
+  printDebugMsg("Start searching for new models");
 
   dumpModelsCurrentlyCapturing();
 
-  Promise
-    .try(function() {
-      return login();
-    })
+  Promise.try(function() {
+    return login();
+  })
     .then(function() {
       return getLiveModels();
     })
     .then(function(liveModels) {
-      printDebugMsg('createCaptureProcess');
+      printDebugMsg("createCaptureProcess");
       return Promise.all(liveModels.map(createCaptureProcess));
     })
     .then(function() {
-      printDebugMsg('checkCaptureProcess');
+      printDebugMsg("checkCaptureProcess");
       return Promise.all(modelsCurrentlyCapturing.map(checkCaptureProcess));
     })
     .catch(function(err) {
@@ -328,7 +362,11 @@ function mainLoop() {
     .finally(function() {
       dumpModelsCurrentlyCapturing();
 
-      printMsg('Done, will search for new models in ' + config.modelScanInterval + ' second(s).');
+      printMsg(
+        "Done, will search for new models in " +
+          config.modelScanInterval +
+          " second(s)."
+      );
 
       setTimeout(mainLoop, config.modelScanInterval * 1000);
     });
